8ef74b1925e5c9315d2f616f7ea4ce3b965dee36
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2024-03-07 19:31:06 -0800

    Beta AcousticMobilePush build: 3.9.3
    Beta AcousticMobilePush Change Notes:
    Bug Fixes
    
    - None
    
    Improvements
    
    - Deploy to npm at ....
    
    Our environment for this release:
    
    - Android Studio Hedgehog | 2023.1.1 Patch 1
    - Xcode version: 15
    - MacOS 14.0
    - iOS
    - Android target API level 21 to 34
    - Cordova version: 11.1.0 (cordova-lib@11.1.0)
    - Cordova Android plugin version: 10.1.2
    - Cordova iOS plugin version: 6.3.0
    - Gradle version: 7.6
    
    Known Issues
    
    - None.

--- applications/samples/Sample/postinstall_cordova.sh
+++ applications/samples/Sample/postinstall_cordova.sh
@@ -22,48 +22,48 @@ cordova plugin add cordova-acoustic-mobile-push-sdk-beta --variable CUSTOM_ACTIO
 
 echo "Adding FCM plugin"
 echo ""
-cordova plugin add file:../../../plugins/cordova-acoustic-mobile-push-plugin-fcm
+cordova plugin add cordova-acoustic-mobile-push-plugin-fcm-beta
 
 echo "Adding Location plugin"
 echo ""
-cordova plugin add file:../../../plugins/cordova-acoustic-mobile-push-plugin-location --variable SYNC_RADIUS=10000 --variable SYNC_INTERVAL=60
+cordova plugin add cordova-acoustic-mobile-push-plugin-location-beta --variable SYNC_RADIUS=10000 --variable SYNC_INTERVAL=60
 
 echo "Adding Beacon plugin"
 echo ""
-cordova plugin add file:../../../plugins/cordova-acoustic-mobile-push-plugin-beacon --variable UUID=21A9A7F4-0DD8-46B7-9D4D-379F3C4AF77D
+cordova plugin add cordova-acoustic-mobile-push-plugin-beacon-beta --variable UUID=21A9A7F4-0DD8-46B7-9D4D-379F3C4AF77D
 
 echo "Adding Inapp plugin"
 echo ""
-cordova plugin add file:../../../plugins/cordova-acoustic-mobile-push-plugin-inapp 
+cordova plugin add cordova-acoustic-mobile-push-plugin-inapp-beta 
 
 echo "Adding Inbox plugin"
 echo ""
-cordova plugin add file:../../../plugins/cordova-acoustic-mobile-push-plugin-inbox
+cordova plugin add cordova-acoustic-mobile-push-plugin-inbox-beta
 
 echo "Adding display web, dial & snooze plugin"
 echo ""
-cordova plugin add file:../../../plugins/cordova-acoustic-mobile-push-plugin-displayweb
-cordova plugin add file:../../../plugins/cordova-acoustic-mobile-push-plugin-dial
-cordova plugin add file:../../../plugins/cordova-acoustic-mobile-push-plugin-snooze
+cordova plugin add cordova-acoustic-mobile-push-plugin-displayweb-beta
+cordova plugin add cordova-acoustic-mobile-push-plugin-dial-beta
+cordova plugin add cordova-acoustic-mobile-push-plugin-snooze-beta
 
 echo "Adding Calendar plugin"
 echo ""
-cordova plugin add file:../../../plugins/cordova-acoustic-mobile-push-plugin-calendar
+cordova plugin add cordova-acoustic-mobile-push-plugin-calendar-beta
 
 echo "Adding display geofence"
 echo ""
-cordova plugin add file:../../../plugins/cordova-acoustic-mobile-push-plugin-geofence
+cordova plugin add cordova-acoustic-mobile-push-plugin-geofence-beta
 
 echo "Adding iOS notification service"
 echo ""
-cordova plugin add file:../../../plugins/cordova-acoustic-mobile-push-plugin-ios-notification-service
+cordova plugin add cordova-acoustic-mobile-push-plugin-ios-notification-service-beta
 
 echo "Adding iOS Action Menu"
 echo ""
-cordova plugin add file:../../../plugins/cordova-acoustic-mobile-push-plugin-action-menu
+cordova plugin add cordova-acoustic-mobile-push-plugin-action-menu-beta
 
 echo "Adding iOS Passbook"
 echo ""
-cordova plugin add file:../../../plugins/cordova-acoustic-mobile-push-plugin-passbook
+cordova plugin add cordova-acoustic-mobile-push-plugin-passbook-beta
 
 
