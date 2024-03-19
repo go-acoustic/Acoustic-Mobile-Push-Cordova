0c4b0a1cd2a570bc325a516ea4ece097b7f8fa85
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2024-03-19 12:00:51 -0700

    Beta AcousticMobilePush build: 3.9.17
    Beta AcousticMobilePush Change Notes:
    Bug Fixes
    
    - None
    
    Improvements
    
    - Deploy to npm at ....
    
    Our environment for this release:
    
    - Android Studio Hedgehog | 2023.1.1 Patch 1
    - Xcode version: 15
    - MacOS 14.0
    - iOS
    - Android target API level 21 to 34
    - Cordova version: 11.1.0 (cordova-lib@11.1.0)
    - Cordova Android plugin version: 10.1.2
    - Cordova iOS plugin version: 6.3.0
    - Gradle version: 7.6
    
    Known Issues
    
    - None.

--- plugins/cordova-acoustic-mobile-push-sdk/manual/MCEPlugin.js
+++ plugins/cordova-acoustic-mobile-push-sdk/manual/MCEPlugin.js
@@ -36,7 +36,7 @@ var MCEPlugin;
   @param {SdkVersionCallback} callback The callback that handles the response
   */
     MCEPlugin.getPluginVersion = function (callback) {
-        callback("3.9.16");
+        callback("3.9.17");
     };
     /**
   Allow Cordova developer to know when registration occurs.
