74ba30841fbb3ecf3a91ceb5559979d2152b08f3
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2024-03-19 20:25:51 -0700

    Beta AcousticMobilePush build: 3.9.21
    Beta AcousticMobilePush Change Notes:
    Bug Fixes
    
    - None
    
    Improvements
    
    - Deploy to npm at ....
    
    Our environment for this release:
    
    - Android Studio Hedgehog | 2023.1.1 Patch 1
    - Xcode version: 15
    - MacOS 14.0
    - iOS
    - Android target API level 21 to 34
    - Cordova version: 11.1.0 (cordova-lib@11.1.0)
    - Cordova Android plugin version: 10.1.2
    - Cordova iOS plugin version: 6.3.0
    - Gradle version: 7.6
    
    Known Issues
    
    - None.

--- applications/samples/Sample/postinstall_cordova.sh
+++ applications/samples/Sample/postinstall_cordova.sh
@@ -1,3 +1,20 @@
+# Setup settings to run application
+# Used in cordova-acoustic-mobile-push-sdk
+customAction="openInboxMessage sendEmail"
+androidAppkey="YOUR ANDROID APPKEY HERE"
+iOSAppkey="YOUR iOS APPKEY HERE"
+iOSProdkey="YOUR iOS APPKEY HERE"
+serverUrl="https://mobile-sdk-lib-xx-y.brilliantcollector.com"
+logLevel="verbose"
+mceCanSyncOverride=true
+
+# Used in cordova-acoustic-mobile-push-plugin-location
+syncRadius=10000 
+syncInterval=60
+
+# Used in cordova-acoustic-mobile-push-plugin-beacon
+uuid=21A9A7F4-0DD8-46B7-9D4D-379F3C4AF77D
+
 echo "Adding android platform"
 cordova platform add android
 echo ""
@@ -7,8 +24,7 @@ cordova platform add ios
 echo ""
 
 echo "Adding Core Acoustic SDK plugin"
-# # cordova plugin add cordova-acoustic-mobile-push-sdk-beta --variable CUSTOM_ACTIONS="openInboxMessage sendEmail" --variable ANDROID_APPKEY="YOUR ANDROID APPKEY HERE" --variable IOS_DEV_APPKEY="YOUR iOS APPKEY HERE" --variable IOS_PROD_APPKEY="YOUR iOS APPKEY HERE" --variable SERVER_URL=https://mobile-sdk-lib-xx-y.brilliantcollector.com --variable LOGLEVEL=error --variable MCE_CAN_SYNC_OVERRIDE=true --force
-cordova plugin add cordova-acoustic-mobile-push-sdk-beta --variable CUSTOM_ACTIONS="openInboxMessage sendEmail" --variable ANDROID_APPKEY="YOUR ANDROID APPKEY HERE" --variable IOS_DEV_APPKEY="YOUR iOS APPKEY HERE" --variable IOS_PROD_APPKEY="YOUR iOS APPKEY HERE" --variable SERVER_URL=https://mobile-sdk-lib-xx-y.brilliantcollector.com --variable LOGLEVEL=error --variable MCE_CAN_SYNC_OVERRIDE=true --force
+cordova plugin add cordova-acoustic-mobile-push-sdk-beta --variable CUSTOM_ACTIONS="$customAction" --variable ANDROID_APPKEY="$androidAppkey" --variable IOS_DEV_APPKEY="$iOSAppkey" --variable IOS_PROD_APPKEY="$iOSProdkey" --variable SERVER_URL="$serverUrl" --variable LOGLEVEL=error"$logLevel" --variable MCE_CAN_SYNC_OVERRIDE="$mceCanSyncOverride" --force
 echo ""
 
 echo "Adding FCM plugin"
@@ -16,12 +32,11 @@ cordova plugin add cordova-acoustic-mobile-push-plugin-fcm-beta
 echo ""
 
 echo "Adding Location plugin"
-# # # cordova plugin add cordova-acoustic-mobile-push-plugin-location-beta --variable SYNC_RADIUS=10000 --variable SYNC_INTERVAL=60
-cordova plugin add cordova-acoustic-mobile-push-plugin-location-beta --variable SYNC_RADIUS=10000 --variable SYNC_INTERVAL=60
+cordova plugin add cordova-acoustic-mobile-push-plugin-location-beta --variable SYNC_RADIUS="$syncRadius" --variable SYNC_INTERVAL="$syncInterval"
 echo ""
 
 echo "Adding Beacon plugin"
-cordova plugin add cordova-acoustic-mobile-push-plugin-beacon-beta --variable UUID=21A9A7F4-0DD8-46B7-9D4D-379F3C4AF77D
+cordova plugin add cordova-acoustic-mobile-push-plugin-beacon-beta --variable UUID="$uuid"
 echo ""
 
 echo "Adding Inapp plugin"
@@ -32,9 +47,15 @@ echo "Adding Inbox plugin"
 cordova plugin add cordova-acoustic-mobile-push-plugin-inbox-beta
 echo ""
 
-echo "Adding display web, dial & snooze plugin"
+echo "Adding display web plugin"
 cordova plugin add cordova-acoustic-mobile-push-plugin-displayweb-beta
+echo ""
+
+echo "Adding display dial plugin"
 cordova plugin add cordova-acoustic-mobile-push-plugin-dial-beta
+echo ""
+
+echo "Adding display snooze plugin"
 cordova plugin add cordova-acoustic-mobile-push-plugin-snooze-beta
 echo ""
 
@@ -57,4 +78,3 @@ echo ""
 echo "Adding iOS Passbook"
 cordova plugin add cordova-acoustic-mobile-push-plugin-passbook-beta
 echo ""
-
