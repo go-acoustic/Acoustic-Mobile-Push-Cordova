3c26086ff2547cc039d9cf282731646fd1a8ab9f
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2024-04-09 20:01:32 -0700

    Beta AcousticMobilePush build: 3.9.40
    Beta AcousticMobilePush Change Notes:
    Bug Fixes
    
    - None
    
    Improvements
    
    - Deploy to npm at ....
    
    Our environment for this release:
    
    - Android Studio Hedgehog | 2023.1.1 Patch 1
    - Xcode version: 15
    - MacOS 14.0
    - iOS
    - Android target API level 21 to 34
    - Cordova version: 11.1.0 (cordova-lib@11.1.0)
    - Cordova Android plugin version: 10.1.2
    - Cordova iOS plugin version: 6.3.0
    - Gradle version: 7.6
    
    Known Issues
    
    - None.

--- applications/samples/Sample/postinstall_cordova.sh
+++ applications/samples/Sample/postinstall_cordova.sh
@@ -1,20 +1,3 @@
-# Setup settings to run application
-# Used in cordova-acoustic-mobile-push-sdk
-customAction="openInboxMessage sendEmail"
-androidAppkey="YOUR ANDROID APPKEY HERE"
-iOSAppkey="YOUR iOS APPKEY HERE"
-iOSProdkey="YOUR iOS APPKEY HERE"
-serverUrl="https://mobile-sdk-lib-xx-y.brilliantcollector.com"
-logLevel="verbose"
-mceCanSyncOverride=true
-
-# Used in cordova-acoustic-mobile-push-plugin-location
-syncRadius=10000 
-syncInterval=60
-
-# Used in cordova-acoustic-mobile-push-plugin-beacon
-myUUID="21A9A7F4-0DD8-46B7-9D4D-379F3C4AF77D"
-
 echo "Adding android platform"
 cordova platform add android
 echo ""
@@ -24,57 +7,5 @@ cordova platform add ios
 echo ""
 
 echo "Adding Core Acoustic SDK plugin"
-cordova plugin add cordova-acoustic-mobile-push-sdk-beta --variable CUSTOM_ACTIONS="$customAction" --variable ANDROID_APPKEY="$androidAppkey" --variable IOS_DEV_APPKEY="$iOSAppkey" --variable IOS_PROD_APPKEY="$iOSProdkey" --variable SERVER_URL="$serverUrl" --variable LOGLEVEL=error"$logLevel" --variable MCE_CAN_SYNC_OVERRIDE="$mceCanSyncOverride" --force
-echo ""
-
-echo "Adding FCM plugin"
-cordova plugin add cordova-acoustic-mobile-push-plugin-fcm-beta
-echo ""
-
-echo "Adding Location plugin"
-cordova plugin add cordova-acoustic-mobile-push-plugin-location-beta --variable SYNC_RADIUS="$syncRadius" --variable SYNC_INTERVAL="$syncInterval"
-echo ""
-
-echo "Adding Beacon plugin"
-cordova plugin add cordova-acoustic-mobile-push-plugin-beacon-beta --variable UUID="$myUUID"
-echo ""
-
-echo "Adding Inapp plugin"
-cordova plugin add cordova-acoustic-mobile-push-plugin-inapp-beta 
-echo ""
-
-echo "Adding Inbox plugin"
-cordova plugin add cordova-acoustic-mobile-push-plugin-inbox-beta
-echo ""
-
-echo "Adding display web plugin"
-cordova plugin add cordova-acoustic-mobile-push-plugin-displayweb-beta
-echo ""
-
-echo "Adding display dial plugin"
-cordova plugin add cordova-acoustic-mobile-push-plugin-dial-beta
-echo ""
-
-echo "Adding display snooze plugin"
-cordova plugin add cordova-acoustic-mobile-push-plugin-snooze-beta
-echo ""
-
-echo "Adding Calendar plugin"
-cordova plugin add cordova-acoustic-mobile-push-plugin-calendar-beta
-echo ""
-
-echo "Adding display geofence"
-cordova plugin add cordova-acoustic-mobile-push-plugin-geofence-beta
-echo ""
-
-echo "Adding iOS notification service"
-cordova plugin add cordova-acoustic-mobile-push-plugin-ios-notification-service-beta
-echo ""
-
-echo "Adding iOS Action Menu"
-cordova plugin add cordova-acoustic-mobile-push-plugin-action-menu-beta
-echo ""
-
-echo "Adding iOS Passbook"
-cordova plugin add cordova-acoustic-mobile-push-plugin-passbook-beta
+npm install plugin add cordova-acoustic-mobile-push-sdk-beta
 echo ""
--- plugins/cordova-acoustic-mobile-push-sdk/www/js/MCEPlugin.js
+++ plugins/cordova-acoustic-mobile-push-sdk/www/js/MCEPlugin.js
@@ -36,7 +36,7 @@ var MCEPlugin;
   @param {SdkVersionCallback} callback The callback that handles the response
   */
     MCEPlugin.getPluginVersion = function (callback) {
-        callback("3.9.39");
+        callback("3.9.40");
     };
     /**
   Allow Cordova developer to know when registration occurs.
