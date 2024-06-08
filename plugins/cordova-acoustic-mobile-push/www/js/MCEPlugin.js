4b10317a4f451abf282b8b863276a9ed56b91014
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2024-06-07 23:06:41 -0700

    Beta AcousticMobilePush build: 3.9.114
    Beta AcousticMobilePush Change Notes:
    Bug Fixes
    
    - None
    
    Improvements
    
    - Added new apis for Android Notifications:
        - setIconColor
        - setLargeIcon
        - setSound
        - setVibrateEnabled
        - setVibrationPattern
        - setLightsEnabled
        - setLights
        - addFlags
    - 'cordova-acoustic-mobile-push-plugin-dial' has moved over to base.
    - Deploy to npm at https://www.npmjs.com/package/cordova-acoustic-mobile-push-sdk.
    - 'cordova-acoustic-mobile-push-plugin-fcm' has moved over to base.
    
    Our environment for this release:
    
    - Android Studio Jellyfish | 2023.3.1 Patch 1
    - Xcode version: 15.4
    - MacOS 14.5
    - iOS 17.x
    - Android target API level 24 to 34
    - Cordova version: 12.0.0
    - Cordova Android plugin version: 13.0.0
    - Cordova iOS plugin version: 7.1.0
    - Gradle version: 7.6
    - Node 18
    
    Known Issues
    
    - If using an emulator for testing on Android
        - We noticed that there are issues with emulators using api 24 which are missing play services.
          The lowest level api working emulator was api 25 with Android 7.1.1 arm64-v8a image.
        - There are issues with older emulators that do not have correct codecs to replay videos. There
          are no issues with latest versions.
    - The sample application is not updating UI for Beacon tests for iOS or Android.
    - If using a gps spoofer for testing beacons for Android, you might have issues on dwell or exit.
    - For iOS notification service, you will need to follow https://developer.goacoustic.com/acoustic-campaign/docs/add-the-ios-notification-service-framework-to-your-cordova-project

--- plugins/cordova-acoustic-mobile-push/www/js/MCEPlugin.js
+++ plugins/cordova-acoustic-mobile-push/www/js/MCEPlugin.js
@@ -36,7 +36,7 @@ var MCEPlugin;
   @param {SdkVersionCallback} callback The callback that handles the response
   */
     MCEPlugin.getPluginVersion = function (callback) {
-        callback("3.9.113");
+        callback("3.9.114");
     };
     /**
   Allow Cordova developer to know when registration occurs.
